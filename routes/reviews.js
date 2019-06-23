2e20b688eccefd67024d597892e25bdb314ada7f
alfanfirmansyah <alfanfputra@gmail.com>
2019-06-23 17:57:34 +0700

    update fitur 2

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,7 +5,7 @@ var con = require('../db.js')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   if(req.query.foods_id){
-    var sql = "Select * from reviews Where foods_id = " +req.query.foods_id
+    var sql = "SELECT reviews.*, SUBSTRING_INDEX(comment, ' ', 6) AS judulcomment from reviews WHERE foods_id = " +req.query.foods_id
     
       }
       else{
@@ -20,13 +20,28 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/notify', function(req, res, next) {
+
+  var paramdate = req.body.date
+  var sql = "SELECT * FROM `reviews` WHERE created_at = "+mysql.escape(paramdate)
+    con.query(sql, function (err, result) {
+      if(result.length == 0){
+        console.log("No Result: " + result);
+        res.send(result[0]);
+      }
+      else{
+        console.log("Result: " + result);
+        res.send(result[0]);
+      }
+    });
+  });
+
 router.post('/', function(req, res, next) {
 var date = new Date();
 
-con.query("Insert into reviews values (?,?,?,?,?,?,?)",[
+con.query("Insert into reviews values (?,?,?,?,?,?)",[
   null,
   req.body.name,
-  req.body.email,
   req.body.comment,
   req.body.rating,
   req.body.foods_id,
